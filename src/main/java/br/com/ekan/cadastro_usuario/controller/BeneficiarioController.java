78571b54ee987a7253a09a1f805630c50b590caf
GabrielSVA <gabrielsv10.gs@gmail.com>
2024-06-02 21:35:33 -0300

    adição de documentacao swagger

--- src/main/java/br/com/ekan/cadastro_usuario/controller/BeneficiarioController.java
+++ src/main/java/br/com/ekan/cadastro_usuario/controller/BeneficiarioController.java
@@ -5,6 +5,8 @@ import br.com.ekan.cadastro_usuario.dto.BeneficiarioDTO;
 import br.com.ekan.cadastro_usuario.model.Beneficiario;
 import br.com.ekan.cadastro_usuario.model.Documento;
 import br.com.ekan.cadastro_usuario.service.BeneficiarioService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -14,31 +16,32 @@ import java.util.Optional;
 
 @RestController
 @RequestMapping("/ekan-club")
+@Tag(name= "ekan-club")
 public class BeneficiarioController {
 
     @Autowired
     private BeneficiarioService beneficiarioService;
 
-    // funcionando
+    @Operation(summary = "Traz todos os usuários cadastrados no banco", method = "GET")
     @GetMapping("/beneficiarios/salvos")
     private List<Beneficiario> beneficiarios() {
         return beneficiarioService.findAll();
     }
 
-    //funcionando
+    @Operation(summary = "Traz os usuários cadastrados  filtrados por ID", method = "GET")
     @GetMapping("/beneficiarios/{id}")
     private Optional<Beneficiario> beneficiariosPorID(@PathVariable("id") Long id) {
         return beneficiarioService.findById(id);
     }
 
 
-    // funcionando
+    @Operation(summary = "Traz os documentos dos usuários  filtrados por ID de usuário", method = "GET")
     @GetMapping("/beneficiarios/documentos/{beneficiarioId}")
      public Optional<List<Documento>> listarDocumentos(@PathVariable Long beneficiarioId) {
         return beneficiarioService.findDocumentsById(beneficiarioId);
     }
 
-
+    @Operation(summary = "Atualiza os dados dos usuários cadastrados", method = "PUT")
     @PutMapping("/beneficiarios/update/{beneficiarioID}")
     public ResponseEntity<Beneficiario> updateBeneficiario(@PathVariable Long beneficiarioID, @RequestBody Beneficiario updatedBeneficiario) {
         Beneficiario savedBeneficiario = beneficiarioService.updateBeneficiario(beneficiarioID, updatedBeneficiario);
@@ -46,14 +49,15 @@ public class BeneficiarioController {
     }
 
 
-    //funcionando
+    @Operation(summary = "Deleta um beneficiário selecionado por ID", method = "DELETE")
     @DeleteMapping("/beneficiarios/{beneficiarioID}")
     public ResponseEntity<BeneficiarioDTO> delete(@PathVariable long beneficiarioID){
         beneficiarioService.delete(beneficiarioID);
         return ResponseEntity.noContent().build();
     }
 
-    //funcionando
+
+    @Operation(summary = "Adiciona um novo usuário", method = "POST")
     @PostMapping("/beneficiarios/save")
     public ResponseEntity<Beneficiario> save(@RequestBody Beneficiario beneficiario) {
         Beneficiario beneficiariosalvo = beneficiarioService.save(beneficiario);
--- src/main/java/br/com/ekan/cadastro_usuario/repository/BeneficiarioRepository.java
+++ src/main/java/br/com/ekan/cadastro_usuario/repository/BeneficiarioRepository.java
@@ -2,7 +2,9 @@ package br.com.ekan.cadastro_usuario.repository;
 
 import br.com.ekan.cadastro_usuario.model.Beneficiario;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface BeneficiarioRepository extends JpaRepository <Beneficiario, Long>{
 
 }
